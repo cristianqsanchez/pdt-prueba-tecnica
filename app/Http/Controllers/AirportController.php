e00762bed172b6acfbd59ad6ae86a25c0dc0938b
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-14 15:46:51 -0500

    feat: add cache to airport controller

--- app/Http/Controllers/AirportController.php
+++ app/Http/Controllers/AirportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class AirportController extends Controller
@@ -13,18 +14,27 @@ class AirportController extends Controller
             'code' => 'required|string'
         ]);
 
-        try {
+        $cacheKey = $this->generateCacheKey($request);
+
+        $airports = Cache::remember($cacheKey, 10080, function () use ($request) {
             $response = Http::post('https://staging.travelflight.aiop.com.co/api/airports?code=' . $request->code);
 
             if ($response->failed()) {
                 return response()->json(['error' => 'Error fetching airports'], 500);
             }
 
-            $airports = $response->json();
+            return $response->json();
+        });
 
+        try {
             return response()->json($airports, 200);
         } catch (\Exception) {
             return response()->json(['error' => 'Internal Server Error'], 500);
         }
+    }
+
+    private function generateCacheKey(Request $request)
+    {
+        return md5(json_encode($request->all()));
     }
 }
