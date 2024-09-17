b57bdd3f24600daa7c9eab505e128e8e657c38a6
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-16 23:20:45 -0500

    feat: add reserves endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AirportController;
 use App\Http\Controllers\FlightController;
+use App\Http\Controllers\ReserveController;
 
 Route::post('/airports', [AirportController::class, 'getAirportsByCode']);
 Route::post('/flights', [FlightController::class, 'getFlights']);
+Route::post('/reserves', [ReserveController::class, 'store']);
