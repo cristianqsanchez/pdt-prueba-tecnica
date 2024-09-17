9af40f3bf621d19363f2d4b911853b662c365657
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-16 23:21:19 -0500

    chore: add new eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,7 @@ export default antfu({
   rules: {
     'style/jsx-quotes': ['warn', 'prefer-single'],
     'ts/consistent-type-definitions': ['warn', 'type'],
+    'style/brace-style': ['off'],
+    'antfu/top-level-function': ['off'],
   },
 })
