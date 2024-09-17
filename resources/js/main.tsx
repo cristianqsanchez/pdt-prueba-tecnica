11c57d0a9314fabd0b2d3a78d3d51ff805ab60bf
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-17 00:58:16 -0500

    wrap app with journey provider & ui

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
+import { Header } from './components/header'
+import { FlightsProvider } from './flights/context'
+import { Toaster } from './ui/sonner'
+import { ThemeProvider } from './ui/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <FlightsProvider>
+        <Header />
+        <App />
+        <Toaster />
+      </FlightsProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
