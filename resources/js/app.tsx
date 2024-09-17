2c2769919bd6fe50ee32b20abe3cc8c5ba8fa502
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-17 01:49:04 -0500

    feat: add app routes

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Router } from 'wouter'
+import { FlightResults } from './flights/components/flight-results'
+import { Landing } from './flights/pages/landing'
 
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Route path='/' component={Landing} />
+      <Route path='/results' component={FlightResults} />
+    </Router>
   )
 }
